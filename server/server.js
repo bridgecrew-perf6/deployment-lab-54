070cd7c86741ac54670e0c4a8d271f68f3039f79
Ty Hill <hill.tyj22@gmail.com>
2022-02-23 14:10:10 -0700

    css

--- server/server.js
+++ server/server.js
@@ -8,6 +8,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
 })
 
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../index.css'))
+// })
+
 const port = process.env.PORT || 4200
 
 app.listen(port, () => {
